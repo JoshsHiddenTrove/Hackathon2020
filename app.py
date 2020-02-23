c6b2e05d3ff4cddfeded90aea0c74afb06dfd739
Ari Schermer <47006598+arischermer@users.noreply.github.com>
2020-02-23 02:35:08 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import smartmonkey
+
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=["POST","GET"])
+#num monkeys = args[0]
+#mutation = args[1]
+#interval = args[2]
+# Newpop(
+#     NewGoal
+#     NewNgram
+def show_front():
+    if(request.method == "GET"):
+        if(len(request.args.to_dict()) > 0):
+            print(request.args)
+            smartmonkey.Newpop(request.args.get('num_monkeys'))
+            smartmonkey.NewGoal(request.args.get('mutation'))
+            smartmonkey.NewNgram(request.args.get('interval'))
+
+            result = smartmonkey.result
+            print("Ayyyyyyyyyyyyyyyyy")
+            print(result)
+            print(result)
+            
+            return render_template('front.html', d = result)
+    
+    return render_template('front.html')
+
+
+if __name__ == '__main__':
+    t = smartmonkey.Individual("helloWord")
+    smartmonkey.main()
+    app.run()
